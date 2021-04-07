e43d15c0d85c246264afe6001bf9b064c3487dbe
greggarrett <gregorygarrett@ufl.edu>
2021-04-07 13:00:34 -0400

    Initial design of login page. TODO: Finish authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/welcomPage.dart';
 import 'screens/graphPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  //TODO: Setup actual authentication behind the scenes
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +21,6 @@ class MyApp extends StatelessWidget {
       routes: {
         WelcomePage.route:(context)=>WelcomePage(),
         graphPage.route:(context)=>graphPage(),
-
       },
     );
   }
--- lib/screens/welcomPage.dart
+++ lib/screens/welcomPage.dart
@@ -1,12 +1,49 @@
 import 'package:dte_epic_interface/components/bigButton.dart';
 import 'package:dte_epic_interface/screens/graphPage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class WelcomePage extends StatelessWidget {
+//TODO: Decide if using stateless or stateful widget
+class WelcomePage extends StatefulWidget {
   static final route = "welcome";
-  ///TODO: Implement FireBase Auth here
-  ///TODO: Check if the user is already logged in, if so then go directly to the main app (skipping the welcome and login screens)
-  ///TODO: hghd
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+//TODO: Check if the user is already logged in, if so then go directly to the main app (skipping the welcome and login screens)
+class _LoginPageState extends State<WelcomePage> {
+
+  String _email;
+  String _password;
+
+  Future<void> _createUser() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth
+          .instance
+          .createUserWithEmailAndPassword(email: _email, password: _password);
+      print("User: $userCredential");
+    } on FirebaseAuthException catch (e) {
+      print("error: $e");
+    } catch (e) {
+      print("error: $e");
+    }
+  }
+
+  Future<void> _login() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth
+          .instance
+          .signInWithEmailAndPassword(email: _email, password: _password);
+      print("User: $userCredential");
+      //TODO: Create LoginPage and uncomment / check that this button works
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>graphPage()));
+    } on FirebaseAuthException catch (e) {
+      print("error: $e");
+    } catch (e) {
+      print("error: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,16 +55,44 @@ class WelcomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            BigButton(
-              buttonText: "Login",
-              onClick: (){
-                ///TODO: Create LoginPage and uncomment / check that this button works
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>graphPage()));
-              },
+            TextField(
+                onSubmitted: (value) {
+                  _email = value;
+                },
+                onChanged: (value) {
+                  _email = value;
+                },
+                decoration: InputDecoration(
+                    hintText: "Enter Email"
+                )
+            ),
+            TextField(
+                onSubmitted: (value) {
+                  _password = value;
+                },
+                onChanged: (value) {
+                  _password = value;
+                },
+                decoration: InputDecoration(
+                    hintText: "Enter Password"
+                )
+            ),
+            Column(
+              children: [
+                BigButton(
+                  buttonText: "Login",
+                  onClick: _login,
+                ),
+                MaterialButton(
+                  //TODO: Right now makes new account based on what's in email/pass fields, probably want a createUser page
+                  onPressed: _createUser,
+                  child: Text("Create New Account"),
+                )
+              ]
             )
           ],
         ),
-    ),
+      ),
       ),
     );
   }
